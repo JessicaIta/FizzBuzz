cf267b279fa19f4e66a6c6ad3af562ff5ac48114
Raphael Pantaleão <raphaelpanta@gmail.com>
2016-10-24 20:29:00 -0300

    Second test done

--- src/fuzzbuzz.js
+++ src/fuzzbuzz.js
@@ -1,5 +1,13 @@
 function FizzBuzz(numbers) {
-  return numbers;
+  var result = [];
+  for(var i = 0; i < numbers.length; i++) {
+    if (numbers[i]%3 === 0){
+      result.push("Fizz");
+    }else{
+      result.push(numbers[i]);
+    }
+  }
+  return result;
 }
 
 exports.FizzBuzz = FizzBuzz;
--- test/fizzbuzz-test.js
+++ test/fizzbuzz-test.js
@@ -4,8 +4,10 @@ var f = require('../src/fuzzbuzz.js');
 describe('FizzBuzz', function() {
 
   it('should return [1] when call #FizzBuzz([1])', function() {
-      assert.deepEqual([1], f.FizzBuzz([1]), "should return the same array because there's no number divible by 3 or 5 or both");
+      assert.deepEqual([1], f.FizzBuzz([1]), "should return the same array because there's no number divisible by 3 or 5 or both");
   });
 
-  
+  it ('should return ["Fizz"] when call #FizzBuzz([3])',function(){
+      assert.deepEqual(["Fizz"], f.FizzBuzz([3]),"should return Fizz because the number is divisible by 3");
+  });
 });
